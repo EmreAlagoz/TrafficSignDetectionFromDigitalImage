bb60d17ae66ba18dccb5339487c35678922c97a1
EmreAlagoz <emrealagoz123@gmail.com>
2024-01-25 21:19:42 +0300

    comment

--- utils.py
+++ utils.py
@@ -39,33 +39,6 @@ def resize_and_pad_image(
     jitter=[512, 1024],
     stride=128.0
 ):
-    """Resizes and pads image while preserving aspect ratio.
-
-    1. Resizes images so that the shorter side is equal to `min_side`
-    2. If the longer side is greater than `max_side`, then resize the image
-      with longer side equal to `max_side`
-    3. Pad with zeros on right and bottom to make the image shape divisible by
-    `stride`
-
-    Arguments:
-      image: A 3-D tensor of shape `(height, width, channels)` representing an
-        image.
-      min_side: The shorter side of the image is resized to this value, if
-        `jitter` is set to None.
-      max_side: If the longer side of the image exceeds this value after
-        resizing, the image is resized such that the longer side now equals to
-        this value.
-      jitter: A list of floats containing minimum and maximum size for scale
-        jittering. If available, the shorter side of the image will be
-        resized to a random value in this range.
-      stride: The stride of the smallest feature map in the feature pyramid.
-        Can be calculated using `image_size / feature_map_size`.
-
-    Returns:
-      image: Resized and padded image.
-      image_shape: Shape of the image before padding.
-      ratio: The scaling factor used to resize the image
-    """
     image_shape = tf.cast(tf.shape(image)[:2], dtype=tf.float32)
     if jitter is not None:
         min_side = tf.random.uniform((), jitter[0], jitter[1], dtype=tf.float32)
@@ -84,14 +57,6 @@ def resize_and_pad_image(
 
 
 def swap_xy(boxes):
-    """Swaps order the of x and y coordinates of the boxes.
-
-    Arguments:
-      boxes: A tensor with shape `(num_boxes, 4)` representing bounding boxes.
-
-    Returns:
-      swapped boxes with shape same as that of boxes.
-    """
     return tf.stack([boxes[:, 1], boxes[:, 0], boxes[:, 3], boxes[:, 2]], axis=-1)
 
 
@@ -112,16 +77,6 @@ def normalize_bbox(bbox, w=1622, h=626):
 
 
 def convert_to_xywh(boxes):
-    """Changes the box format to center, width and height.
-
-    Arguments:
-      boxes: A tensor of rank 2 or higher with a shape of `(..., num_boxes, 4)`
-        representing bounding boxes where each box is of the format
-        `[xmin, ymin, xmax, ymax]`.
-
-    Returns:
-      converted boxes with shape same as that of boxes.
-    """
     return tf.concat(
         [(boxes[..., :2] + boxes[..., 2:]) / 2.0, boxes[..., 2:] - boxes[..., :2]],
         axis=-1,
@@ -129,16 +84,6 @@ def convert_to_xywh(boxes):
 
 
 def convert_to_corners(boxes):
-    """Changes the box format to corner coordinates
-
-    Arguments:
-      boxes: A tensor of rank 2 or higher with a shape of `(..., num_boxes, 4)`
-        representing bounding boxes where each box is of the format
-        `[x, y, width, height]`.
-
-    Returns:
-      converted boxes with shape same as that of boxes.
-    """
     return tf.concat(
         [boxes[..., :2] - boxes[..., 2:] / 2.0, boxes[..., :2] + boxes[..., 2:] / 2.0],
         axis=-1,
@@ -146,19 +91,6 @@ def convert_to_corners(boxes):
 
 
 def compute_iou(boxes1, boxes2):
-    """Computes pairwise IOU matrix for given two sets of boxes
-
-    Arguments:
-      boxes1: A tensor with shape `(N, 4)` representing bounding boxes
-        where each box is of the format `[x, y, width, height]`.
-        boxes2: A tensor with shape `(M, 4)` representing bounding boxes
-        where each box is of the format `[x, y, width, height]`.
-
-    Returns:
-      pairwise IOU matrix with shape `(N, M)`, where the value at ith row
-        jth column holds the IOU between ith box and jth box from
-        boxes1 and boxes2 respectively.
-    """
     boxes1_corners = convert_to_corners(boxes1)
     boxes2_corners = convert_to_corners(boxes2)
     lu = tf.maximum(boxes1_corners[:, None, :2], boxes2_corners[:, :2])
@@ -223,12 +155,6 @@ def visualize_detections(
 
 
 def try_ignore_error(func, *argv):
-    """
-    Try and ignore error
-    @params:
-      + func: function
-      + *argv: arguments of func
-    """
     try:
         func(*argv)
     except Exception as e:
